94556788bca51283c9fd9a686207ba7b416e9ae1
blunt <bluntoriginal444@gmail.com>
2026-06-19 22:38:17 +0530

    made arrow to deal damage upon enemy hit, have set two origin place one for vertical and another for horizontal.

--- Assets/Scripts/Player/Arrow.cs
+++ Assets/Scripts/Player/Arrow.cs
@@ -6,10 +6,32 @@ public class Arrow : MonoBehaviour
     public Vector2 direction = Vector2.right;
     public float lifeSpawn;
     public float speed;
+    public LayerMask enemyLayer;
+    public int damage;
+    public float knockBackForce;
+    public float knockBackTime;
+    public float stunTime;
 
-    void Start()
+    public void Launch(Vector2 dir)
     {
+        Debug.Log("Launch called with direction: " + dir);
+        direction = dir;
         rb.linearVelocity = direction * speed;
+        RotateArrow();
         Destroy(gameObject, lifeSpawn);
+    }
+    private void RotateArrow()
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((enemyLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            collision.gameObject.GetComponent<Enemy_Health>().ChangeHealth(-damage);
+            collision.gameObject.GetComponent<Enemy_Knockback>().Knockback(transform, knockBackForce, knockBackTime, stunTime);
+        }
     }
 }
--- Assets/Scripts/Player/Player_Bow.cs
+++ Assets/Scripts/Player/Player_Bow.cs
@@ -3,19 +3,71 @@ using UnityEngine;
 
 public class Player_Bow : MonoBehaviour
 {
-    public Transform launchPoint;
+    public Transform launchPointHorizontal;
+    public Transform launchPointVertical;
+    public Transform currentLaunchPoint;
     public GameObject arrowPrefab;
     public Vector2 aimDirection;
+    public float shootCooldown;
+
+    private float shootTimer;
+
+    private void Start()
+    {
+        currentLaunchPoint = launchPointHorizontal;
+    }
 
     void Update()
     {
-        if (Input.GetButtonDown("Shoot"))
+        shootTimer -= Time.deltaTime;
+        HandleAiming();
+        if (Input.GetButtonDown("Shoot") && shootTimer <= 0)
         {
             Shoot();
         }
     }
     public void Shoot()
     {
-        Instantiate(arrowPrefab, launchPoint.position, Quaternion.identity);
+        Arrow arrow = Instantiate(arrowPrefab, currentLaunchPoint.position, Quaternion.identity).GetComponent<Arrow>();
+        arrow.Launch(aimDirection);
+        shootTimer = shootCooldown;
+
+        if (arrow == null)
+        {
+            Debug.LogError("Arrow component not found on prefab!");
+            return;
+        }
+        if (currentLaunchPoint == null)
+        {
+            Debug.LogError("currentLaunchPoint is null!");
+            return;
+        }
+    }
+    private void HandleAiming()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            aimDirection = new Vector2(horizontal, vertical).normalized;
+            if (vertical > 0)
+            {
+                currentLaunchPoint = launchPointVertical;
+                Vector3 pos = launchPointVertical.localPosition;
+                launchPointVertical.localPosition = new Vector3(pos.x, Mathf.Abs(pos.y), pos.z);
+            }
+            else if (vertical < 0)
+            {
+                currentLaunchPoint = launchPointVertical;
+                Vector3 pos = launchPointVertical.localPosition;
+                launchPointVertical.localPosition = new Vector3(pos.x, -Mathf.Abs(pos.y), pos.z);
+            }
+            else
+            {
+                currentLaunchPoint = launchPointHorizontal;
+            }
+
+        }
     }
 }
