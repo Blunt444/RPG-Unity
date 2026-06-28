b8802ce0e72d21a7eb459630cfa20060bca12b16
blunt <bluntoriginal444@gmail.com>
2026-06-28 19:09:08 +0530

    added a fade animation so whenever a scene loads it plays it. Made delay so the animation is still visible. Fixed the confiner from missing when different scenes load

--- Assets/Scripts/SceneChange/SceneChanger.cs
+++ Assets/Scripts/SceneChange/SceneChanger.cs
@@ -1,15 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     public string sceneToLoad;
+    public Animator anim;
+    public float fadeTime = 1;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            anim.Play("FadeToWhite");
+            StartCoroutine(DelayFade());    
         }
+    }
+
+    IEnumerator DelayFade()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
