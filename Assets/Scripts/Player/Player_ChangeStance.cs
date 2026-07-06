ad27aa3f9fb383c2f79d07854d0ed82e912844cc
blunt <bluntoriginal444@gmail.com>
2026-07-06 23:03:30 +0530

    added a stance changing transistion

--- Assets/Scripts/Player/Player_ChangeStance.cs
+++ Assets/Scripts/Player/Player_ChangeStance.cs
@@ -1,16 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class Player_ChangeStance : MonoBehaviour
 {
     public Player_Combat playerCombat;
     public Player_Bow playerBow;
+    public Animator anim;
+
+
+    private float transistionTime = 0.5f;
+    private float waitForFadeOut = 1.0f;
+    private bool isTransistioning = false;
 
     void Update()
     {
-        if (Input.GetButtonDown("ToggleStance"))
+        if (Input.GetButtonDown("ToggleStance") && !isTransistioning)
         {
-            playerCombat.enabled = !playerCombat.enabled;
-            playerBow.enabled = !playerBow.enabled;
+            StartCoroutine(AnimationTransistion());
         }
+    }
+
+    private IEnumerator AnimationTransistion()
+    {
+        isTransistioning = true;
+        anim.Play("FadeIn");
+        Time.timeScale = 0;
+        
+        yield return new WaitForSecondsRealtime(transistionTime);
+
+        playerCombat.enabled = !playerCombat.enabled;
+        playerBow.enabled = !playerBow.enabled;
+
+        anim.Play("FadeOut");
+        Time.timeScale = 1;
+
+        yield return new WaitForSecondsRealtime(waitForFadeOut);
+        
+        isTransistioning = false;
     }
 }
--- TODO.md
+++ TODO.md
@@ -6,6 +6,7 @@ Add a activeTalker in dialgoue manager so when click to continue is implemented 
 
 completed : changed the player sprites for melee
 
+add override controller for player for different sprites.
 when player transition to a new mode like archer add a fade in and out animation.
 Player bow shoot method dir can be changed after shooting which shouldn't happen.
 When player shoot method called in y axis x = 0 or x ~ 1 to 2 the script get confused which animation to play.
