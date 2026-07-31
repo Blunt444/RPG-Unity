5f466dfd60c5c1a70ef12b2f261af26507b8bc18
Blunt444 <bluntoriginal444@gmail.com>
2026-07-31 23:01:47 +0530

    added a tree manager which holds override controller and stump

---
+++ Assets/Scripts/Tree/TreeManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public struct TreeOverrides
+{
+    public string key;
+    public AnimatorOverrideController overrider;
+    public Sprite[] choppedSprite;
+}
+
+public class TreeManager : MonoBehaviour
+{
+    public static TreeManager Instance;
+
+    [SerializeField]
+    List<TreeOverrides> list = new List<TreeOverrides>();
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public TreeOverrides RandomTreeVariant()
+    {
+        return list[UnityEngine.Random.Range(0, list.Count)];
+    }
+}
